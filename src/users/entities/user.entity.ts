b754b0c2bdab5b4c562e0d457724bf9309158a2f
GwangCheonLee <raitmid56@gmail.com>
2024-11-29 20:25:10 +0900

    Refactor: Simplify User entity by removing optional fields and roles

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -6,8 +6,6 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import {IsEmail} from 'class-validator';
-import {UserRole} from '../enum/user-role.enum';
-import {Exclude} from 'class-transformer';
 
 /**
  * User Entity
@@ -17,36 +15,12 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({name: 'oauth_provider', nullable: true})
-  oauthProvider?: string | null;
-
   @Column({name: 'email', unique: true})
   @IsEmail()
   email: string;
 
-  @Exclude()
-  @Column({name: 'password', nullable: true})
-  password?: string | null;
-
   @Column({name: 'nickname'})
   nickname: string;
-
-  @Column({name: 'profile_image', nullable: true})
-  profileImage?: string | null;
-
-  @Column({
-    type: 'enum',
-    enum: UserRole,
-    default: [UserRole.USER],
-    array: true,
-  })
-  roles: UserRole[];
-
-  @Column({name: 'two_factor_authentication_secret', nullable: true})
-  twoFactorAuthenticationSecret?: string | null;
-
-  @Column({name: 'is_active', default: true})
-  isActive: boolean;
 
   @CreateDateColumn()
   createdAt: Date;
--- src/users/enum/user-role.enum.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 사용자의 역할을 정의한 enum
- */
-export enum UserRole {
-  USER = 'USER',
-  ADMIN = 'ADMIN',
-}
