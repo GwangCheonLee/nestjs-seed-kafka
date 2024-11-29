29549c1919927cb4f7aa9b7270ec7addb348a94c
GwangCheonLee <raitmid56@gmail.com>
2024-11-29 20:31:32 +0900

    feat: implement processUserRegistrationEvent in KafkaConsumerService

--- src/kafka/consumer/kafka-consumer.service.ts
+++ src/kafka/consumer/kafka-consumer.service.ts
@@ -1,4 +1,8 @@
 import {Injectable, Logger} from '@nestjs/common';
+import {UserRegistrationMessage} from '../../users/interfaces/user-registration-message.interface';
+import {UserRegistrationDto} from '../../users/dto/user-registration.dto';
+import {transformAndValidate} from '../../common/utils/validation.util';
+import {UserService} from '../../users/user.service';
 
 /**
  * Kafka 컨슈머 서비스
@@ -9,5 +13,18 @@ import {Injectable, Logger} from '@nestjs/common';
 export class KafkaConsumerService {
   private readonly logger = new Logger(KafkaConsumerService.name);
 
-  constructor() {}
+  constructor(private readonly userService: UserService) {}
+
+  async processUserRegistrationEvent(message: UserRegistrationMessage) {
+    const userRegistrationDto: UserRegistrationDto = await transformAndValidate(
+      UserRegistrationDto,
+      message,
+    );
+
+    await this.userService.signUp(userRegistrationDto);
+
+    this.logger.log(
+      `Received user registration: ${JSON.stringify(userRegistrationDto)}`,
+    );
+  }
 }
