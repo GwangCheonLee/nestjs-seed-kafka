dc236b544aa36fd7710658c65c410fd5ae9d0517
GwangCheonLee <raitmid56@gmail.com>
2024-11-29 20:30:15 +0900

    feat: Import type ormModule in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,16 @@ import {validationSchemaConfig} from './common/config/validation.config';
 import {getEnvPath} from './common/config/env-path.config';
 import {KafkaConsumerModule} from './kafka/consumer/kafka-consumer.module';
 import {KafkaProducerModule} from './kafka/producer/kafka-producer.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {TypeormConfig} from './common/config/typeorm.config';
 
 @Module({
   imports: [
     KafkaProducerModule,
     KafkaConsumerModule,
+    TypeOrmModule.forRootAsync({
+      useClass: TypeormConfig,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: getEnvPath(),
